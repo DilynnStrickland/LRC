4522ff1d2b8b7ca3a47c77831c3c1bfc01cc5534
Dilynn Strickland <dilynn.strickla@smail.astate.edu>
2022-03-21 12:29:36 -0500

    schema.sql complete

--- Database/schema.sql
+++ Database/schema.sql
@@ -1,9 +1,12 @@
 CREATE TABLE IF NOT EXISTS GameTable {
     userID TEXT PRIMARY KEY,
-    tableID INTEGER NOT NULL
+    tableID INTEGER NOT NULL,
+    FOREIGN KEY (userID) REFERENCES Users(userID)
 };
 
 CREATE TABLE IF NOT EXISTS Users {
     userID TEXT PRIMARY KEY,
-    
+    username TEXT UNIQUE NOT NULL,
+    hash TEXT UNIQUE NOT NULL,
+    winStreak INTEGER DEFAULT 0 NOT NULL CHECK(winStreak >= 0)
 };
