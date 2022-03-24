c75402cd92aa0d81d28d7083777329fa1af1bc2f
Bradley Hale <bradley.hale@smail.astate.edu>
2022-03-24 14:08:51 -0500

    wrote user validator and userModel, username/password validation may need changing

--- Models/userModel.js
+++ Models/userModel.js
@@ -2,3 +2,37 @@
 const db = require("./db");
 const crypto = require("crypto");
 const argon2 = require("argon2");
+
+
+async function addUser(username,password) {
+    const uuid = crypto.randomUUID();
+    const sql = `INSERT INTO Users (userID, username, hash)
+    VALUES (@userID, @username, @hash)`;
+    const stmt = db.prepare(sql);
+    try {
+        const hash = await argon2.hash(password);
+        stmt.run({
+            "userID":uuid,
+            "username":username,
+            "hash":hash,
+        });
+        return true;
+    } catch(err) {
+        console.error(err);
+        return false;
+    }
+}
+
+function getUserByUsername(username) {
+    const sql = `SELECT * FROM Users WHERE username = @username`;
+    const stmt = db.prepare(sql);
+    const record = stmt.get({
+        "username":username
+    });
+    return record;
+}
+
+module.exports = {
+    addUser,
+    getUserByUsername
+}
--- Validators/userValidator.js
+++ Validators/userValidator.js
@@ -1,2 +1,34 @@
 "use strict";
 const joi = require("joi");
+
+const validateOps = {
+    abortEarly: false,
+    stripUnknown: true,
+    errors: {
+        escapeHtml: true
+    }
+};
+
+const registerSchema = joi.object({
+    username:joi.string()
+    .min(3)
+    .token()
+    .required(),
+    password: joi.string()
+    .min(6)
+    .required()
+});
+
+function validateRegisterBody( req, res, next) {
+    const {value, error} = registerSchema.validate(req.body, validateOps);
+    if(error) {
+        const errorMessages = error.details.map( detail => detail.message);
+        return res.status(400).json({"error":errorMessages});
+    }
+    req.body = value;
+    next();
+}
+
+module.exports = {
+    validateRegisterBody
+}
