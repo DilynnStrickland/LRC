1eebf77c9b9a24cf9b6dfa681217246604b5415b
BradleyHale <bradh2018@gmail.com>
2022-03-28 10:42:19 -0500

    Update userController.js

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,3 +1,46 @@
 "use strict";
 const argon2 = require("argon2");
 const userModel = require("../Models/userModel");
+
+async function createNewUser (req,res) {
+    const {username,password} = req.body;
+    const didCreateUser = await userModel.addUser(username,password);
+    if(!didCreateUser) {
+        return res.sendStatus(409);
+    }
+    res.sendStatus(201);
+}
+
+async function login (req,res) {
+    const {username,password} = req.body;
+    const user = userModel.getUserByUsername(username);
+
+    if(!user) {
+        return res.sendStatus(400);
+    }
+    const {hash} = user;
+
+    if(await argon2.verify(hash,password)) {
+        req.session.regenerate( (error) => {
+            if(error) {
+                console.error(error);
+                return res.sendStatus(500);
+            }
+            req.session.isLoggedIn = true;
+            req.session.user = {"username": username, "userID": userID};
+            res.sendStatus(200);
+        });
+    }
+    else {
+        res.sendStatus(400);
+    }
+}
+
+
+
+
+
+module.exports = {
+    createNewUser,
+    login
+}
