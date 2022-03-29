b7b3d2044b72ded5ff62c22f8a8a6774828547ca
Dilynn Strickland <dilynn.strickla@smail.astate.edu>
2022-03-29 13:14:44 -0500

    index.html work

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 "use strict";
 require("dotenv").config();
-const session = require("express-session");
-const redis = require("redis");
-const RedisStore = require("connect=redis")(session);
 const express = require("express");
 const app = express();
+
+// Allow access to static resources in the public directory
+app.use(express.static("public", {index: "index.html", extensions: ["html"]}));
+
+// The maximum request body size is 100 kilobytes; however, my word list was
+// ~150kb. So I just doubled the request body size limit
+app.use(express.json({limit: '200kb'}));
+app.use(express.urlencoded({ extended: false }));
 
 
 /********************************************************
