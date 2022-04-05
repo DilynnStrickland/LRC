437901c17aa7705110fa9250e9a6098a6b32bc24
BradleyHale <bradh2018@gmail.com>
2022-04-05 16:35:35 -0500

    added roll function

--- Controllers/gameController.js
+++ Controllers/gameController.js
@@ -2,3 +2,21 @@
 
 const gameModel = require("../Models/gameModel");
 
+function roll(credits) {
+    let dice;
+    if(credits >= 3) {
+        dice = 3;
+    }
+    else if(credits == 2) {
+        dice = 2;
+    }
+    else if(credits == 1) {
+        dice = 1;
+    }
+    let results = [dice];
+    return results;
+}
+
+module.exports = {
+    roll,
+}
