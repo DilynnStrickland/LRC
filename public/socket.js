00ad2779c30b9c56eaa0ff17019c767b2b92640c
Bradley Hale <bradley.hale@smail.astate.edu>
2022-04-24 19:21:33 -0500

    added some things to message and whisper

--- public/socket.js
+++ public/socket.js
@@ -4,6 +4,9 @@
 const socket = new WebSocket();  // host name goes in the parenthesis
 
 const playBtn = playBtn.getByElementId("playBtn");
+const messageBtn = messageBtn.getByElementID("messateBtn");
+const messages = document.querySelector('#messages');
+const messageBox = document.querySelector('#messageBox');
 
 playBtn.addEventListener("click", (event) =>{
     playBtn.disabled = true;
@@ -16,20 +19,43 @@ socket.addEventListener("open", (event) => {
     }));
 });
 
+
+messageBtn.addEventListener("click", (event) =>{
+    event.preventDefault();
+    if(!ws) {
+        showMessage("no websocket connection ");
+        return;
+    }
+    
+});
+
 socket.addEventListener("message", (event) =>{
+    event.preventDefault();
     const message = parseJSON(message);
 
     if(message.cmd === "error"){
-
+        
     }else if(message.cmd === "message"){
 
+       ws.send(message.messageSent);
+       showMessage(message.messageSent);
+
     }else if(message.cmd === "whisper"){
+
+        ws.send(message.messageSent);
+        showMessage(message.messageSent);
 
     }else if(message.cmd === "update"){
         
     }
 
 });
+
+function showMessage(message) {
+    messages.textContent += `\n\n${message}`;
+    messages.scrollTop = messages.scrollHeight;
+    messageBox.value = '';
+}
 
 function parseJSON(data) {
     try {
